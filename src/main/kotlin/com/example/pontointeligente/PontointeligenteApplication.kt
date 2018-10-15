4ad507513ef84bc74e9d62cc679cb537d9f5b4f6
Leticia Lima <leticia.lima@zup.com.br>
2018-10-15 16:07:21 -0300

    crud lancamento

--- src/main/kotlin/com/example/pontointeligente/PontointeligenteApplication.kt
+++ src/main/kotlin/com/example/pontointeligente/PontointeligenteApplication.kt
@@ -1,11 +1,46 @@
 package com.example.pontointeligente
 
+import com.example.pontointeligente.documents.Empresa
+import com.example.pontointeligente.documents.Funcionario
+import com.example.pontointeligente.enums.PerfilEnum
+import com.example.pontointeligente.repositories.EmpresaRepository
+import com.example.pontointeligente.repositories.FuncionarioRepository
+import com.example.pontointeligente.repositories.LancamentoRepository
+import com.example.pontointeligente.utils.SenhaUtils
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class PontointeligenteApplication
+class PontointeligenteApplication(val empresaRepository: EmpresaRepository,
+                                  val funcionarioRepository: FuncionarioRepository,
+                                  val lancamentoRepository: LancamentoRepository) : CommandLineRunner {
+
+    override fun run(vararg args: String?) {
+        empresaRepository.deleteAll()
+        funcionarioRepository.deleteAll()
+        lancamentoRepository.deleteAll()
+
+        val empresa: Empresa = Empresa("Empresa", "10443887000146")
+        empresaRepository.save(empresa)
+
+        val admin: Funcionario = Funcionario("Admin", "admin@empresa.com",
+                SenhaUtils().gerarBcrypt("123456"), "25708317000",
+                PerfilEnum.ROLE_ADMIN, empresa.id!!)
+        funcionarioRepository.save(admin)
+
+        val funcionario: Funcionario = Funcionario("Funcionario",
+                "funcionario@empresa.com", SenhaUtils().gerarBcrypt("123456"),
+                "44325441557", PerfilEnum.ROLE_USUARIO, empresa.id!!)
+        funcionarioRepository.save(funcionario)
+
+        System.out.println("Empresa ID: " + empresa.id)
+        System.out.println("Admin ID: " + admin.id)
+        System.out.println("Funcionario ID: " + funcionario.id)
+
+    }
+
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(PontointeligenteApplication::class.java, *args)
--- src/main/kotlin/com/example/pontointeligente/dtos/CadastroPFDto.kt
+++ src/main/kotlin/com/example/pontointeligente/dtos/CadastroPFDto.kt
@@ -9,11 +9,11 @@ import org.hibernate.validator.constraints.br.CPF
 data class CadastroPFDto (
 
         @get:NotEmpty(message = "Nome não pode ser vazio.")
-        @get:Length(min = 3, max = 200, message = "Nome deve conter entre 3 e 200 caracteres.")
+        @get:Length(min = 3, max = 200, message = "Nome deve conter entre {min} e {max} caracteres.")
         val nome: String = "",
 
         @get:NotEmpty(message = "Email não pode ser vazio.")
-        @get:Length(min = 5, max = 200, message = "Email deve conter entre 5 e 200 caracteres.")
+        @get:Length(min = 5, max = 200, message = "Email deve conter entre {min} e {max} caracteres.")
         @get:Email(message="Email inválido.")
         val email: String = "",
 
